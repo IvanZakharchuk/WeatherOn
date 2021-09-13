a68bdbaa8c960a5229cf3f72623e38474762c52e
Іван Захарчук <john.zacharchuk@gmail.com>
2021-09-13 15:25:58 +0300

    Coordinator creating

---
+++ WeatherOn/WeatherOn/Source/Coordinators/BaseCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  BaseCoordinator.swift
+//  WeatherOn
+//
+//  Created by Іван Захарчук on 13.09.2021.
+//
+
+import Foundation
+import UIKit
+
+open class BaseCoordinator: UINavigationController {
+    
+    // MARK: -
+    // MARK: ViewLifeCycle
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.prepare()
+        self.setupNavigation()
+    }
+    
+    // MARK: -
+    // MARK: Private
+    
+    private func setupNavigation() {
+        navigationItem.hidesBackButton = true
+        navigationItem.backBarButtonItem?.tintColor = .white
+        navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationBar.shadowImage = UIImage()
+    }
+    
+    // MARK: -
+    // MARK: Overriding
+    
+    func prepare() {
+        
+    }
+}
---
+++ WeatherOn/WeatherOn/Source/Coordinators/WeatherCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherCoordinator.swift
+//  WeatherOn
+//
+//  Created by Іван Захарчук on 13.09.2021.
+//
+
+import Foundation
+
+class WeatherCoordinator: BaseCoordinator {
+    
+}
